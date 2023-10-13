7b61bed65670572aa19afb0ec790a1c557040a10
adrianadisenoyweb <adrianadisenoyweb@gmail.com>
2023-10-12 22:05:14 -0400

    cambios-1210

--- js/main.js
+++ js/main.js
@@ -8,6 +8,59 @@
         
     });
 
+    //submenu plantillas    
+    $('.menu-plantillas').click(function() {
+      $('.items-plantillas').addClass('show');
+    });
+    //cerrar submenu plantillas
+    $('.items-plantillas a').click(function() {
+        $('.items-plantillas').removeClass('show'); 
+    });
+
+    $('.item-redes').click(function() {
+      $('.plantillas-tarjetas-agradecimiento').hide();
+      $('.plantillas-redes-sociales').show();
+    });
+
+    $('.item-agradecimiento').click(function() {
+      $('.plantillas-redes-sociales').hide();
+      $('.plantillas-tarjetas-agradecimiento').show()
+    });
+
+    //submenu servicios
+    $('.menu-servicios').click(function() {
+      $('.items-servicios').addClass('show');
+    });
+    //cerrar submenu plantillas
+    $('.items-servicios a').click(function() {
+        $('.items-servicios').removeClass('show'); 
+    });
+
+    //paginacion plantillas redes sociales
+    $('.uno').click(function(e) {
+        e.preventDefault();
+        $('.pagina-2').hide();
+        $('.dos').removeClass('pagina-seleccionada');
+      
+        $('.pagina-1').show();      
+        $('.uno').addClass('pagina-seleccionada');
+    });
+
+    $('.dos').click(function(e) {
+        e.preventDefault();
+        $('.pagina-1').hide();
+        $('.uno').removeClass('pagina-seleccionada');
+
+        $('.pagina-2').show();
+        $('.dos').addClass('pagina-seleccionada');
+    });
+
+   
+
+    // if($('.dropdown-menu').hasClass('dropdown-menu show')){
+    //     $('.dropdown-menu').removeClass('show');
+    // }
+
 //Carrusel testimonios
     $('.testimonial-carousel').owlCarousel({
         autoplay: true,
